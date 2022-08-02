347074f2c1bf9c42d2d0f635c8bb4aaf81e63112
TheTrueZK <thetruezk@gmail.com>
2022-08-02 13:14:06 -0500

    set up intial file structure

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const createHTML = require('./src/createHTML');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+
 function Employee(name, id, email) {
     this.name = name;
     this.id = id;
