e0b3ec02721180f65a1c151d465605f4d9d1d33a
TheTrueZK <thetruezk@gmail.com>
2022-08-03 16:19:37 -0500

    wip prompts

--- index.js
+++ index.js
@@ -33,10 +33,44 @@ const addEmployee = () => {
             type: 'input'
             name: 'id'
             message: "Enter the employee's email",
-            validate: email => {
-                valid = expect(email).toEqual(expect.any(String));
-                if (valid)
+            // validate: email => {
+                // valid = expect(email).toEqual(expect.any(String));
+                // if (valid)
+                //finish later
+            // }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Enter the employee's github",
+            when: (input) => input.role === "Engineer",
+            validate: nameInput => {
+                if (nameInput ) {
+                    return true;
+                } else {
+                    console.log ("Enter a github username")
+                }
             }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Enter the employee's school",
+            when: (input) => input.role === "Intern",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Enter the employee's school")
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'newEmployee',
+            message: 'Would you like to add another employee?',
+            default: false
         }
+
     ])
 }
